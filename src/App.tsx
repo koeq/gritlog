0f0f9a1854de903337862023a192c06cf2bf36aa
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2022-05-21 18:38:20 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [text, setText] = useState<string>();
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(event.currentTarget.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        width: "100%",
+        height: "500px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>backslash</h1>
+      <br />
+      <textarea
+        onChange={handleChange}
+        value={text}
+        name="training"
+        id="training"
+        cols={30}
+        rows={10}
+      ></textarea>
+      <br />
+      <input type="submit" />
+
+      <br></br>
+      <br></br>
+
+      {text && (
+        <div>
+          <table style={{ border: "1px solid grey" }}>
+            <tr>
+              <th style={{ borderBottom: "1px solid grey" }}>
+                {new Date().toLocaleDateString()}
+              </th>
+            </tr>
+            <tr>
+              <td>{text}</td>
+            </tr>
+          </table>
+        </div>
+      )}
     </div>
   );
 }
