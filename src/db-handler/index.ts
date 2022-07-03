8a19bd40686eb668eac8eb1ecb0991a72ea14878
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2022-07-03 16:20:42 +0200

    feat: type db-handler

--- src/db-handler/index.ts
+++ src/db-handler/index.ts
@@ -1,33 +1,35 @@
-export {};
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyEventQueryStringParameters,
+  APIGatewayProxyResult,
+} from "aws-lambda";
+
 const AWS = require("aws-sdk");
 const RDS = new AWS.RDSDataService();
-
 const userPath = "/user";
+const trainingPath = "/training";
+const exercisePath = "/exercise";
 
-exports.handler = async (event: any) => {
-  // Log the entire event passed in
-  console.log(JSON.stringify(event, null, 2));
+interface JsonResponse {
+  statusCode: number;
+  headers: {
+    "Content-Type": string;
+  };
+  body: string;
+}
 
-  let response;
-  switch (true) {
-    case event.httpMethod === "GET" && event.path === userPath:
-      response = await getUser(event.queryStringParameters.userId);
-      break;
-    case event.httpMethod === "POST" && event.path === userPath:
-      console.log(event.body);
-      response = await addUser(JSON.parse(event.body));
-      break;
-
-    default:
-      response = buildResponse(404, "404 not found");
+const getUser = async (
+  queryStringParameters: APIGatewayProxyEventQueryStringParameters | null
+) => {
+  if (!queryStringParameters || !queryStringParameters.userID) {
+    return buildResponse(404, "Missing query parameter");
   }
 
-  return response;
-};
+  let { userID } = queryStringParameters;
+  const parsedUserID = parseInt(userID);
 
-const getUser = async (id: number) => {
-  // TODO: implement a more secure way the string to avoid SQL injection
-  const sql = `SELECT * FROM users WHERE userID = ${id}`;
+  // TO DO: implement a more secure way the string to avoid SQL injection
+  const sql = `SELECT * FROM users WHERE userID = ${parsedUserID}`;
 
   try {
     const params = {
@@ -47,11 +49,24 @@ const getUser = async (id: number) => {
   }
 };
 
-const addUser = async (body: any) => {
-  return buildResponse(200, body);
+const addUser = async (body: string | null) => {
+  if (!body) {
+    return buildResponse(404, "Missing body");
+  }
+
+  // TO DO: implement a more secure way the string to avoid SQL injection
+  const sql = `INSERT INTO user (userID, email) values()`;
+
+  try {
+    return buildResponse(201, JSON.parse(body));
+  } catch (err) {
+    console.log(err);
+
+    return buildResponse(404, err);
+  }
 };
 
-const buildResponse = (statusCode: number, body: any) => {
+const buildResponse = (statusCode: number, body: any): JsonResponse => {
   return {
     statusCode: statusCode,
     headers: {
@@ -59,4 +74,28 @@ const buildResponse = (statusCode: number, body: any) => {
     },
     body: JSON.stringify(body),
   };
+};
+
+exports.handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  console.log(JSON.stringify(event, null, 2));
+  let response: JsonResponse;
+
+  switch (true) {
+    case event.httpMethod === "GET" && event.path === userPath:
+      event.queryStringParameters;
+      response = await getUser(event.queryStringParameters);
+      break;
+
+    case event.httpMethod === "POST" && event.path === userPath:
+      response = await addUser(event.body);
+      break;
+
+    default:
+      response = buildResponse(404, "404 not found");
+  }
+
+  console.log(event);
+  return response;
 };
