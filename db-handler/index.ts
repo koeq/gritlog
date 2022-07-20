a2334e81cb52c47bc1f322d13a34e2be24f967eb
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2022-07-20 08:54:39 +0200

    feat: add "debug mode" to db-handler

--- db-handler/index.ts
+++ db-handler/index.ts
@@ -20,61 +20,80 @@ const authPath = "/auth";
 exports.handler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  if (!secretArn || !resourceArn) {
-    return buildResponse(500, "Missing aws arn");
-  }
-
-  const params: RDSDataService.ExecuteStatementRequest = {
-    secretArn,
-    resourceArn,
-    database,
-    sql: "",
-  };
-
-  console.log(JSON.stringify(event, null, 2));
-  let response: JsonResponse = { body: "", headers: {}, statusCode: 0 };
-
-  if (event.path === userPath) {
-    switch (event.httpMethod) {
-      case "GET":
-        response = await getUser(event.queryStringParameters, params);
-        break;
-
-      case "POST":
-        // TO DO: CHECK
-        // is this even needed anymore?
-        break;
-
-      case "DELETE":
-        response = await deleteUser(event.queryStringParameters, params);
-        break;
-
-      default:
-        response = buildResponse(404, "404 not found");
+  try {
+    if (!secretArn || !resourceArn) {
+      return buildResponse(500, "Missing aws arn");
     }
-  }
+    const params: RDSDataService.ExecuteStatementRequest = {
+      secretArn,
+      resourceArn,
+      database,
+      sql: "",
+    };
 
-  if (event.path === authPath) {
-    switch (event.httpMethod) {
-      case "GET":
-        response = checkAuthentication(event.headers);
-        break;
+    console.log(JSON.stringify(event, null, 2));
+    let response: JsonResponse = { body: "", headers: {}, statusCode: 0 };
 
-      case "POST":
-        const user = await checkForUser(event.body, params);
+    if (event.path === userPath) {
+      console.log("we're in the auth path");
+      switch (event.httpMethod) {
+        case "GET":
+          response = await getUser(event.queryStringParameters, params);
+          break;
 
-        if (user) {
-          response = setAuthCookie(200, event.body);
-        } else {
-          response = await createUser(event.body, params);
-        }
-        break;
+        case "POST":
+          // TO DO: CHECK
+          // is this even needed anymore?
+          break;
 
-      default:
-        response = buildResponse(404, "404 not found");
+        case "DELETE":
+          response = await deleteUser(event.queryStringParameters, params);
+          break;
+
+        default:
+          response = buildResponse(404, "404 not found");
+      }
     }
-  }
 
-  console.log(response);
-  return response;
+    if (event.path === authPath) {
+      console.log("we're in the auth path");
+      switch (event.httpMethod) {
+        case "GET":
+          response = checkAuthentication(event.headers);
+          break;
+
+        case "POST":
+          console.log("we're in POST method");
+          try {
+            console.log("we haven't checked the user yet");
+            const user = await checkForUser(event.body, params);
+            console.log("we checked the user");
+
+            if (user) {
+              response = setAuthCookie(200, event.body);
+              console.log("we have an user an set the cookie");
+            } else {
+              response = await createUser(event.body, params);
+              console.log("we created an user and set the cookie");
+            }
+          } catch (err) {
+            console.log(err);
+
+            response = buildResponse(500, err);
+          }
+          break;
+
+        default:
+          console.log("we're in default");
+          response = buildResponse(404, "404 not found");
+      }
+    }
+
+    console.log("we return");
+    return response;
+  } catch (err) {
+    console.log(err);
+
+    return buildResponse(500, err);
+  }
 };
