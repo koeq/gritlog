2c7a71d1357c17f5b4805d928c7365f26319fdf7
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2022-08-07 21:31:41 +0200

    feat: define test cases for parser bugs

--- src/__tests__/parse.test.ts
+++ src/__tests__/parse.test.ts
@@ -3,13 +3,16 @@ import { parse } from "./../parser";
 let singleLine: string;
 let singleLineSpaceBeforeWeight: string;
 let singleLineLbs: string;
+let exerciseNameOnly: string;
 let multiLine: string;
+let multiLineNoNewline: string;
 
 beforeEach(() => {
   singleLine = "Benchpress 100kg 8/8/8";
   singleLineSpaceBeforeWeight = "Benchpress 100 kg 8/8/8";
   singleLineLbs = "Benchpress 100lbs 8/8/8";
   multiLine = "Squats 100kg 8/8/8 \n Benchpress 100kg 8/8/8";
+  multiLineNoNewline = "Squats 100kg 8/8/8 Benchpress 100kg 8/8/8";
 });
 
 test("No Input", () => {
@@ -46,8 +49,31 @@ test("Single-line. Format: exercise _lbs _/_/_", () => {
   ]);
 });
 
+test("Exercise name only. Format: exercise", () => {
+  expect(parse(exerciseNameOnly)).toEqual([
+    {
+      exerciseName: "Benchpress",
+    },
+  ]);
+});
+
 test("Multi-line. Format: exercise _kg _/_/_", () => {
   expect(parse(multiLine)).toEqual([
+    {
+      exerciseName: "Squats",
+      weight: "100kg",
+      repetitions: "8/8/8",
+    },
+    {
+      exerciseName: "Benchpress",
+      weight: "100kg",
+      repetitions: "8/8/8",
+    },
+  ]);
+});
+
+test("Multi-line no newline between. Format: exercise _kg _/_/_", () => {
+  expect(parse(multiLineNoNewline)).toEqual([
     {
       exerciseName: "Squats",
       weight: "100kg",
