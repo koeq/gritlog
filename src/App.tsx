467c0c5b2c13d7eec5e322617774400db3d70ee0
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2022-09-24 12:31:03 +0200

    feat: add loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { Suspense } from "react";
 import { useAuth } from "./context/auth-provider";
+import { LoadingSpinner } from "./loading-spinner";
 
 const AuthedApp = React.lazy(() => import("./authed-app"));
 const UnauthedApp = React.lazy(() => import("./unauthed-app"));
@@ -8,7 +9,7 @@ export const App = () => {
   const { authed } = useAuth();
 
   return (
-    <Suspense fallback={<pre> ¯\_(ツ)_/¯</pre>}>
+    <Suspense fallback={<LoadingSpinner />}>
       {authed ? <AuthedApp /> : <UnauthedApp />}
     </Suspense>
   );
