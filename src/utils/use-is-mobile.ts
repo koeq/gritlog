a068319001d01ca813f03f3b60ecc46ea467a56e
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2022-10-09 11:05:57 +0200

    feat: only add on enter on mobile

---
+++ src/utils/use-is-mobile.ts
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+export const useIsMobile = () => {
+  const [width, setWidth] = useState<number>(window.innerWidth);
+
+  const handleWindowResize = () => setWidth(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowResize);
+    return () => window.removeEventListener("resize", handleWindowResize);
+  }, []);
+
+  return width <= 780;
+};
