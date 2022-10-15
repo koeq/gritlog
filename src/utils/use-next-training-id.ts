8e0a9546e7c3540cca28efa901c25b853f32501f
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2022-10-15 10:57:01 +0200

    feat: create helper to get nextTrainingId

---
+++ src/utils/use-next-training-id.ts
@@ -0,0 +1,9 @@
+import { Training } from "../../db-handler/types";
+
+export const getNextTrainingId = (trainings: Training[]): number => {
+  if (trainings.length === 0) {
+    return 0;
+  }
+
+  return Math.max(...trainings.map((training) => training.id)) + 1;
+};
