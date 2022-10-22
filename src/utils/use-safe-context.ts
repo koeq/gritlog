2a073a5d66dd6e947add923c8fedbe60f18659ea
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2022-10-22 11:44:05 +0200

    fix: clean up

--- src/utils/use-safe-context.ts
+++ src/utils/use-safe-context.ts
@@ -7,7 +7,7 @@ export function useSafeContext<T>(
   const contextValue = useContext(context);
 
   if (!contextValue) {
-    throw new Error(`No value for provided for context ${contextName}.`);
+    throw new Error(`No value provided for context ${contextName}.`);
   }
 
   return contextValue;
