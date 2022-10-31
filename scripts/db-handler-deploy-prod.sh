27a6b5ba9276d59d2056a9a98262b3106737ed57
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2022-10-31 13:39:53 +0100

    build: update db-handler deployment scripts

--- scripts/db-handler-deploy-prod.sh
+++ scripts/db-handler-deploy-prod.sh
@@ -1,6 +1,6 @@
-tsc db-handler/index.ts
-cd db-handler && zip -r  db-handler.zip * -x *.ts utils/*.ts package-lock.json package.json   && cd ..
-aws lambda update-function-code --function-name db-handler --region eu-central-1 --zip-file fileb://db-handler/db-handler.zip --output text
+tsc lambdas/db-handler/index.ts
+cd lambdas/db-handler && zip -r  db-handler.zip * -x *.ts utils/*.ts package-lock.json package.json   && cd ../..
+aws lambda update-function-code --function-name db-handler --region eu-central-1 --zip-file fileb://lambdas/db-handler/db-handler.zip --output text
 aws lambda wait function-updated --function-name db-handler
 aws lambda update-function-configuration --function-name db-handler --environment Variables={ORIGIN=https://backslash-app.com}
 aws lambda wait function-updated --function-name db-handler
