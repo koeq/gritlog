8bfd88f9a84aef14700b05d6c03ed2f4e22eedb7
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2022-11-02 09:54:13 +0100

    refactor: create auth lambda

---
+++ lambdas/auth-handler/utils/build-response.ts
@@ -0,0 +1,34 @@
+export interface JsonResponse {
+  statusCode: number;
+  headers?: {
+    "Content-Type"?: string;
+    "Access-Control-Allow-Origin"?: string;
+    "Access-Control-Allow-Methods"?: string;
+    "Access-Control-Allow-Headers"?: string;
+    "Access-Control-Allow-Credentials"?: string;
+    "Set-Cookie"?: string;
+  };
+  body: string;
+}
+
+export const buildResponse = (
+  statusCode: number,
+  body: unknown,
+
+  cookie?: {
+    "Set-Cookie": string;
+  }
+): JsonResponse => {
+  return {
+    statusCode: statusCode,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": process.env.ORIGIN,
+      "Access-Control-Allow-Methods": "OPTIONS,POST,GET, PUT, DELETE",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization, x-api-key",
+      "Access-Control-Allow-Credentials": "true",
+      ...cookie,
+    },
+    body: JSON.stringify(body),
+  };
+};
---
+++ lambdas/auth-handler/utils/index.ts
@@ -0,0 +1,7 @@
+export * from "./build-response";
+export * from "./check-for-user";
+export * from "./create-user";
+
+export * from "../../auth-handler/utils/is-user-authenticated";
+export * from "./set-auth-cookie";
+export * from "./delete-auth-cookie";
