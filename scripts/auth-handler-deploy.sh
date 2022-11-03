732840e3f7f1ce927073c207bced2d978aedfb75
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2022-11-03 10:47:45 +0100

    build: add deployment scripts for auth handler

---
+++ scripts/auth-handler-deploy.sh
@@ -0,0 +1,15 @@
+tsc lambdas/auth-handler/index.ts
+cd lambdas/auth-handler && zip -r  auth-handler.zip * -x *.ts utils/*.ts package-lock.json package.json   && cd ../..
+aws lambda update-function-code --function-name auth-handler --region eu-central-1 --zip-file fileb://lambdas/auth-handler/auth-handler.zip --output text
+#DEV
+aws lambda wait function-updated --function-name auth-handler
+aws lambda update-function-configuration --function-name auth-handler --environment Variables={ORIGIN=http://localhost:3000} --output text
+aws lambda wait function-updated --function-name auth-handler
+VERSION=$(aws lambda publish-version --function-name auth-handler --description 'dev' --output text --query Version)
+aws lambda update-alias --function-name auth-handler --name dev --function-version $VERSION
+#PROD
+aws lambda wait function-updated --function-name auth-handler
+aws lambda update-function-configuration --function-name auth-handler --environment Variables={ORIGIN=https://backslash-app.com}
+aws lambda wait function-updated --function-name auth-handler
+VERSION=$(aws lambda publish-version --function-name auth-handler --description 'prod' --output text --query Version)
+aws lambda update-alias --function-name auth-handler --name prod --function-version $VERSION
