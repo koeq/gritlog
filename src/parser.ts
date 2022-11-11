9540a19009da56a5815d7a5e6650c44c3955fdf4
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2022-11-11 08:30:22 +0100

    feat: add barbone error handling

--- src/parser.ts
+++ src/parser.ts
@@ -1,16 +1,24 @@
 export {};
 
-// Parsing:
-//          1. lexical analysis -> creates tokens out of the strings
-//             (e.g Benchpress 100kg 3*8 ---> "Benchpress", "100kg", "3*8")
+// DEFINITIONS
+
+//  lexeme: are the smallest sequence of substrings of the source which still represent something.
+// tokens: a token is made out of a lexeme and additional data to that lexeme (e.g. token type)
 
 interface TokenType {
+  // Single-character Tokens
   readonly asperand: "@";
   readonly forwardSlash: "/";
   readonly star: "*";
   readonly hashtag: "#";
+
+  // Literals
   readonly number: number;
   readonly string: string;
+
+  // Keywords
+  readonly kilograms: "kg";
+  readonly pounds: "lbs";
 }
 
 interface Token {
@@ -20,5 +28,34 @@ interface Token {
   readonly line: number;
 }
 
-//          2. syntactic analysis -> gives the logical meaning to that token
-//             (e.g. 3*8 ---> 8/8/8)
+// ERROR HANDLING:
+function parse(source: string | undefined) {
+  let hadError: boolean = false;
+
+  if (source === undefined) {
+    return;
+  }
+
+  function scanTokens(source: string): Token[] {
+    return [];
+  }
+
+  const tokens: Token[] = scanTokens(source);
+
+  for (const token of tokens) {
+    console.log(tokens);
+  }
+
+  function error(line: number, message: string) {
+    report(line, "", message);
+  }
+
+  function report(line: number, where: string, message: string) {
+    console.log(`[line ${line}] Error ${where}: ${message}`);
+    hadError = true;
+  }
+
+  if (hadError) {
+    return;
+  }
+}
