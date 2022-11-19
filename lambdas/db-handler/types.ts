db365ad704da528b8235d8e62a2fd2be3157ed87
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2022-11-19 15:52:25 +0100

    feat: build headline

--- lambdas/db-handler/types.ts
+++ lambdas/db-handler/types.ts
@@ -1,11 +1,15 @@
+// TODO: check if the types here make sense
 export interface Exercise {
   readonly exerciseName: string | null;
   readonly weight: string | null;
   readonly repetitions: string | null;
 }
 
+export type Headline = string | undefined;
+
 export interface Training {
   readonly date: string;
   readonly id: number;
+  readonly headline: Headline;
   readonly exercises: Exercise[] | undefined;
 }
