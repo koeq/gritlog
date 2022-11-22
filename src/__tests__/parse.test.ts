130493d8efbd764999a69713f28bffdca10e8bc8
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2022-11-22 22:03:51 +0100

    feat: update tests

--- src/__tests__/parse.test.ts
+++ src/__tests__/parse.test.ts
@@ -1,72 +1,130 @@
 import { parse } from "../parser";
 
 let singleLine: string;
-let singleLineSpaceBeforeWeight: string;
+let singleLineDecimalPoint: string;
+let singleLineCommaPoint: string;
+let singleLineWithHeadline: string;
 let singleLineLbs: string;
-let exerciseNameOnly: string;
 let multiLine: string;
+let multiplicator: string;
 
 beforeEach(() => {
-  singleLine = "Benchpress 100kg 8/8/8";
-  singleLineSpaceBeforeWeight = "Benchpress 100 kg 8/8/8";
-  singleLineLbs = "Benchpress 100lbs 8/8/8";
-  exerciseNameOnly = "Benchpress";
-  multiLine = "Squats 100kg 8/8/8 \n Benchpress 100kg 8/8/8";
+  singleLine = "Benchpress @100kg 8/8/8";
+  singleLineDecimalPoint = "Benchpress @100.5kg 8/8/8";
+  singleLineCommaPoint = "Benchpress @100,5kg 8/8/8";
+  singleLineWithHeadline = "#some headline \n Benchpress @100kg 8/8/8";
+  singleLineLbs = "Benchpress @100lbs 8/8/8";
+  multiLine = "Squats @100kg 8/8/8 \n Benchpress @100kg 8/8/8";
+  multiplicator = "Squats @100 3*8";
 });
+
+const singleLineResult = {
+  headline: null,
+  exercises: [
+    {
+      exerciseName: "Benchpress",
+      repetitions: "8/8/8",
+      weight: "100kg",
+    },
+  ],
+};
+
+const singleLineDecimalPointResult = {
+  headline: null,
+  exercises: [
+    {
+      exerciseName: "Benchpress",
+      repetitions: "8/8/8",
+      weight: "100.5kg",
+    },
+  ],
+};
+const singleLineCommaResult = {
+  headline: null,
+  exercises: [
+    {
+      exerciseName: "Benchpress",
+      repetitions: "8/8/8",
+      weight: "100,5kg",
+    },
+  ],
+};
+
+const singleLineWithHeadlineResult = {
+  headline: "some headline",
+  exercises: [
+    {
+      exerciseName: "Benchpress",
+      repetitions: "8/8/8",
+      weight: "100kg",
+    },
+  ],
+};
+
+const singleLineLbsResult = {
+  headline: null,
+  exercises: [
+    {
+      exerciseName: "Benchpress",
+      repetitions: "8/8/8",
+      weight: "100lbs",
+    },
+  ],
+};
+
+const multiplicatorResult = {
+  headline: null,
+  exercises: [
+    {
+      exerciseName: "Squats",
+      repetitions: "8/8/8",
+      weight: "100",
+    },
+  ],
+};
 
 test("No Input", () => {
   expect(parse(undefined)).toBe(undefined);
 });
 
 test("Single-line. Format: exercise _kg _/_/_", () => {
-  expect(parse(singleLine)).toEqual([
-    {
-      exerciseName: "Benchpress",
-      weight: "100kg",
-      repetitions: "8/8/8",
-    },
-  ]);
+  expect(parse(singleLine)).toEqual(singleLineResult);
 });
 
-test("Single-line. Format: exercise _ kg _/_/_", () => {
-  expect(parse(singleLineSpaceBeforeWeight)).toEqual([
-    {
-      exerciseName: "Benchpress",
-      weight: "100kg",
-      repetitions: "8/8/8",
-    },
-  ]);
+test("Single-line decimal point. Format: exercise _._kg _/_/_", () => {
+  expect(parse(singleLineDecimalPoint)).toEqual(singleLineDecimalPointResult);
+});
+
+test("Single-line comma. Format: exercise _,_kg _/_/_", () => {
+  expect(parse(singleLineCommaPoint)).toEqual(singleLineCommaResult);
+});
+
+test("Single-line with headline", () => {
+  expect(parse(singleLineWithHeadline)).toEqual(singleLineWithHeadlineResult);
 });
 
 test("Single-line. Format: exercise _lbs _/_/_", () => {
-  expect(parse(singleLineLbs)).toEqual([
-    {
-      exerciseName: "Benchpress",
-      weight: "100lbs",
-      repetitions: "8/8/8",
-    },
-  ]);
-});
-
-test("Exercise name only. Format: exercise", () => {
-  expect(parse(exerciseNameOnly)).toEqual([
-    {
-      exerciseName: "Benchpress",
-    },
-  ]);
+  expect(parse(singleLineLbs)).toEqual(singleLineLbsResult);
 });
 
 test("Multi-line. Format: exercise _kg _/_/_", () => {
-  expect(parse(multiLine)).toEqual([
-    {
-      exerciseName: "Squats",
-      weight: "100kg",
-      repetitions: "8/8/8",
-    },
-    {
-      exerciseName: "Benchpress",
-      weight: "100kg",
-      repetitions: "8/8/8",
-    },
-  ]);
+  expect(parse(multiLine)).toEqual({
+    headline: null,
+    exercises: [
+      {
+        exerciseName: "Squats",
+        weight: "100kg",
+        repetitions: "8/8/8",
+      },
+      {
+        exerciseName: "Benchpress",
+        weight: "100kg",
+        repetitions: "8/8/8",
+      },
+    ],
+  });
+});
+
+test("Multiplicator. Format: exercise _kg _*_", () => {
+  expect(parse(multiplicator)).toEqual(multiplicatorResult);
 });
