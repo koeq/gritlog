1cd10a86b862bcd423fe3049d724a1a4b6acc097
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2022-11-25 09:13:15 +0100

    feat: add test no forward slashes

--- src/__tests__/parse.test.ts
+++ src/__tests__/parse.test.ts
@@ -3,6 +3,7 @@ import { parse } from "../parser";
 let singleLine: string;
 let singleLineDecimalPoint: string;
 let singleLineCommaPoint: string;
+let singleLineMinimal: string;
 let singleLineWithHeadline: string;
 let singleLineLbs: string;
 let multiLine: string;
@@ -12,6 +13,7 @@ beforeEach(() => {
   singleLine = "Benchpress @100kg 8/8/8";
   singleLineDecimalPoint = "Benchpress @100.5kg 8/8/8";
   singleLineCommaPoint = "Benchpress @100,5kg 8/8/8";
+  singleLineMinimal = "Benchpress @100 8 8 8";
   singleLineWithHeadline = "#some headline \n Benchpress @100kg 8/8/8";
   singleLineLbs = "Benchpress @100lbs 8/8/8";
   multiLine = "Squats @100kg 8/8/8 \n Benchpress @100kg 8/8/8";
@@ -39,6 +41,7 @@ const singleLineDecimalPointResult = {
     },
   ],
 };
+
 const singleLineCommaResult = {
   headline: null,
   exercises: [
@@ -46,6 +49,17 @@ const singleLineCommaResult = {
       exerciseName: "Benchpress",
       repetitions: "8/8/8",
       weight: "100,5kg",
+    },
+  ],
+};
+
+const singleLineMinimalResult = {
+  headline: null,
+  exercises: [
+    {
+      exerciseName: "Benchpress",
+      repetitions: "8 8 8",
+      weight: "100",
     },
   ],
 };
@@ -87,27 +101,31 @@ test("No Input", () => {
   expect(parse(undefined)).toBe(undefined);
 });
 
-test("Single-line. Format: exercise _kg _/_/_", () => {
+test("Single-line. Format: exercise @_kg _/_/_", () => {
   expect(parse(singleLine)).toEqual(singleLineResult);
 });
 
-test("Single-line decimal point. Format: exercise _._kg _/_/_", () => {
+test("Single-line decimal point. Format: exercise @_._kg _/_/_", () => {
   expect(parse(singleLineDecimalPoint)).toEqual(singleLineDecimalPointResult);
 });
 
-test("Single-line comma. Format: exercise _,_kg _/_/_", () => {
+test("Single-line comma. Format: exercise @_,_kg _/_/_", () => {
   expect(parse(singleLineCommaPoint)).toEqual(singleLineCommaResult);
+});
+
+test("Single-line minimal. Format: exercise @_ _ _ _", () => {
+  expect(parse(singleLineMinimal)).toEqual(singleLineMinimalResult);
 });
 
 test("Single-line with headline", () => {
   expect(parse(singleLineWithHeadline)).toEqual(singleLineWithHeadlineResult);
 });
 
-test("Single-line. Format: exercise _lbs _/_/_", () => {
+test("Single-line. Format: exercise @_lbs _/_/_", () => {
   expect(parse(singleLineLbs)).toEqual(singleLineLbsResult);
 });
 
-test("Multi-line. Format: exercise _kg _/_/_", () => {
+test("Multi-line. Format: exercise @_kg _/_/_", () => {
   expect(parse(multiLine)).toEqual({
     headline: null,
     exercises: [
@@ -125,6 +143,6 @@ test("Multi-line. Format: exercise _kg _/_/_", () => {
   });
 });
 
-test("Multiplicator. Format: exercise _kg _*_", () => {
+test("Multiplicator. Format: exercise @_kg _*_", () => {
   expect(parse(multiplicator)).toEqual(multiplicatorResult);
 });
