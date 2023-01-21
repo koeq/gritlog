d4b0d5b1c2c05cf2e4105d75369ce7b0a64c9653
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2023-01-21 17:42:13 +0100

    refactor: update useLocalStorage
    Because the local storage state setter is being passed down to other
    components the custom hook needs to return a stable identity for the
    state setter.

--- src/use-local-storage.ts
+++ src/use-local-storage.ts
@@ -1,24 +1,31 @@
 import { useState } from "react";
 
+function retrieveFromLocalStorage<T>(key: string): T | null {
+  try {
+    const item = localStorage.getItem(key);
+
+    return item && JSON.parse(item);
+  } catch (err) {
+    console.log(err);
+
+    return null;
+  }
+}
+
 export function useLocalStorage<T>(
   key: string,
   defaultValue: T
 ): readonly [T, (value: T | ((val: T) => T)) => void] {
-  const [storedValue, setStoredValue] = useState<T>(() => {
-    try {
-      const item = localStorage.getItem(key);
-      return item ? JSON.parse(item) : defaultValue;
-    } catch (err) {
-      console.log(err);
-      return defaultValue;
-    }
-  });
+  const [storedValue, setStoredValue] = useState<T>(
+    () => retrieveFromLocalStorage(key) || defaultValue
+  );
 
-  // alllow value to be a function to imitate the useState API
   const setValue = (value: T | ((val: T) => T)) => {
     try {
+      // Alllow value to be a function to imitate the useState API
       const valueToStore =
         value instanceof Function ? value(storedValue) : value;
+
       setStoredValue(valueToStore);
       localStorage.setItem(key, JSON.stringify(valueToStore));
     } catch (err) {
