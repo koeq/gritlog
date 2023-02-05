9f489df5ce3e139c76614234c7d466d1a636ea9a
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2023-02-05 13:07:30 +0100

    fix: adjust `TrainingSchema`

--- src/schemas.ts
+++ src/schemas.ts
@@ -3,12 +3,13 @@ import { z } from "zod";
 export const TrainingSchema = z.object({
   date: z.string(),
   id: z.number(),
-  headline: z.union([z.string(), z.null()]),
+  // null types can also be undefined because of changed implementation in the past
+  headline: z.union([z.string(), z.null(), z.undefined()]),
   exercises: z
     .object({
-      exerciseName: z.union([z.string(), z.null()]),
-      weight: z.union([z.string(), z.null()]),
-      repetitions: z.union([z.string(), z.null()]),
+      exerciseName: z.union([z.string(), z.null(), z.undefined()]),
+      weight: z.union([z.string(), z.null(), z.undefined()]),
+      repetitions: z.union([z.string(), z.null(), z.undefined()]),
     })
     .array(),
 });
