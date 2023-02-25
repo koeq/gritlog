2f0ded37e81dba192f9e346beeb3a1715a03cc21
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2023-02-25 15:54:53 +0100

    feat: add back desktop click interaction

--- src/training-table-with-buttons.tsx
+++ src/training-table-with-buttons.tsx
@@ -1,5 +1,7 @@
+import { useRef } from "react";
 import { IoTrashBin } from "react-icons/io5";
 import { MdModeEdit } from "react-icons/md";
+import { useIsMobile } from "./context/is-mobile-provider";
 import "./styles/training-table-with-buttons.css";
 import { TrainingTable } from "./training-table";
 import { Mode, Training } from "./types";
@@ -15,8 +17,29 @@ export const TrainingTableWithButtons = ({
   handleSetEditMode,
   setMode,
 }: TrainingTableProps): JSX.Element | null => {
+  const trainingRef = useRef<HTMLDivElement | null>(null);
+  const isMobile = useIsMobile();
+
+  const handleDesktopClick = () => {
+    const { current: element } = trainingRef;
+
+    if (element) {
+      // The single pixel works because of the scroll-snap styles
+      const leftValue = element.scrollLeft ? -1 : 1;
+
+      element.scrollBy({
+        left: leftValue,
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
-    <div className="training-with-buttons">
+    <div
+      className="training-with-buttons"
+      ref={trainingRef}
+      onClick={isMobile ? undefined : handleDesktopClick}
+    >
       <TrainingTable training={training} />
       <div className="buttons-container">
         <button
