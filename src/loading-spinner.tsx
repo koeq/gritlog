e3c63935c0ddef4d309d92ae170c2b3676506fa6
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2023-03-09 20:56:11 +0100

    feat: update loading spinner

--- src/loading-spinner.tsx
+++ src/loading-spinner.tsx
@@ -2,12 +2,14 @@ import "./styles/loading-spinner.css";
 
 export const LoadingSpinner = (): JSX.Element => {
   return (
-    <div className="lds-container">
-      <div className="lds-ring">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+    <div className="loading-spinner-container">
+      <div className="spinner">
+        <div className="spinner-dot"></div>
+        <div className="spinner-dot"></div>
+        <div className="spinner-dot"></div>
+        <div className="spinner-dot"></div>
+        <div className="spinner-dot"></div>
+        <div className="spinner-dot"></div>
       </div>
     </div>
   );
