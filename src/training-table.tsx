761d7e73ed5002a980abf8f6eb03a240878ea04b
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2023-03-31 09:50:55 +0200

    feat: summarize reps if possible

--- src/training-table.tsx
+++ src/training-table.tsx
@@ -5,45 +5,9 @@ interface TrainingTableProps {
   readonly training: Training;
 }
 
-const TableHeaders = (): JSX.Element => {
-  return (
-    <tr>
-      <th id="exerciseLabel" className="label-header">
-        exercise
-      </th>
-      <th id="weightLabel" className="label-header">
-        weight
-      </th>
-      <th className="label-header">repetitions</th>
-    </tr>
-  );
-};
-
 interface TableValueProps {
   readonly training: Training;
 }
-
-const TableValues = ({ training }: TableValueProps): JSX.Element => {
-  return training.exercises && training.exercises.length > 0 ? (
-    <>
-      {training.exercises.map(
-        ({ exerciseName, weight, repetitions }, index) => (
-          <tr key={index}>
-            <td id="exercise">{exerciseName || "—"}</td>
-            <td id="weight">{weight || "—"}</td>
-            <td id="repetitions">{repetitions || "—"}</td>
-          </tr>
-        )
-      )}
-    </>
-  ) : (
-    <tr>
-      <td id="exercise">—</td>
-      <td id="weight">—</td>
-      <td id="repetitions">—</td>
-    </tr>
-  );
-};
 
 export const TrainingTable = ({
   training,
@@ -71,4 +35,57 @@ export const TrainingTable = ({
       </tbody>
     </table>
   );
+};
+
+const TableHeaders = (): JSX.Element => {
+  return (
+    <tr>
+      <th id="exerciseLabel" className="label-header">
+        exercise
+      </th>
+      <th id="weightLabel" className="label-header">
+        weight
+      </th>
+      <th className="label-header">reps</th>
+    </tr>
+  );
+};
+
+const TableValues = ({ training }: TableValueProps): JSX.Element => {
+  const { exercises } = training;
+
+  return exercises?.length > 0 ? (
+    <>
+      {exercises.map(({ exerciseName, weight, repetitions }, index) => {
+        return (
+          <tr key={index}>
+            <td id="exercise">{exerciseName || "—"}</td>
+            <td id="weight">{weight || "—"}</td>
+            <td id="repetitions">{parseReps(repetitions) || "—"}</td>
+          </tr>
+        );
+      })}
+    </>
+  ) : (
+    <tr>
+      <td id="exercise">—</td>
+      <td id="weight">—</td>
+      <td id="repetitions">—</td>
+    </tr>
+  );
+};
+
+const parseReps = (repetitions: string | null | undefined) => {
+  if (!repetitions) {
+    return;
+  }
+
+  const reps = repetitions.split("/");
+  const firstRep = reps[0];
+
+  if (reps.length > 1 && reps.every((rep) => rep === firstRep)) {
+    return `${reps.length}x${firstRep}`;
+  }
+
+  return repetitions;
 };
