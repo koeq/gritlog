1fd18d931f91f743c73b6813ea860007698f683f
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2023-04-01 10:11:48 +0200

    feat: show percentage change for latest training

--- src/trainings.tsx
+++ src/trainings.tsx
@@ -18,6 +18,84 @@ interface TrainingsProps {
   }: HandleSetEditModeParams) => void;
 }
 
+const parseWeight = (weight: string | null | undefined) => {
+  const match = weight?.match(/\d+/);
+
+  return match ? parseInt(match[0]) : 0;
+};
+
+const parseReps = (reps: string | null | undefined) => {
+  const summedReps = reps
+    ?.split("/")
+    .map((rep) => parseInt(rep))
+    .reduce((acc, rep) => acc + rep);
+
+  return summedReps || 0;
+};
+
+const getWorkPerExercise = (training: Training) =>
+  training.exercises.reduce(
+    (acc: Record<string, number>, { exerciseName, weight, repetitions }) => {
+      const doneWork = parseWeight(weight) * parseReps(repetitions);
+
+      if (!exerciseName) {
+        return acc;
+      }
+
+      if (Object.prototype.hasOwnProperty.call(acc, exerciseName)) {
+        acc[exerciseName] += doneWork;
+      } else {
+        acc[exerciseName] = doneWork;
+      }
+
+      return acc;
+    },
+    {}
+  );
+
+const getLatestPercentageChanges = (
+  latestTraining: Training,
+  trainings: Training[]
+) => {
+  const percentageChanges: Record<string, number> = {
+    trainingId: latestTraining.id,
+  };
+
+  if (trainings.length < 2) {
+    percentageChanges;
+  }
+
+  const exerciseWorkMap = getWorkPerExercise(latestTraining);
+
+  for (const [exercise, work] of Object.entries(exerciseWorkMap)) {
+    if (work === 0) {
+      continue;
+    }
+
+    for (let i = trainings.length - 2; i >= 0; i--) {
+      const prevExerciseWorkMap = getWorkPerExercise(trainings[i]);
+
+      if (
+        !Object.prototype.hasOwnProperty.call(prevExerciseWorkMap, exercise)
+      ) {
+        continue;
+      }
+
+      const prevWork = prevExerciseWorkMap[exercise];
+
+      if (prevWork === 0) {
+        break;
+      }
+
+      const percentageChange = (work / prevWork - 1) * 100;
+      percentageChanges[exercise] = percentageChange;
+      break;
+    }
+  }
+
+  return percentageChanges;
+};
+
 export const Trainings = ({
   dispatch,
   trainings,
@@ -28,25 +106,39 @@ export const Trainings = ({
     return null;
   }
 
+  const latestTraining = trainings[trainings.length - 1];
+
+  const percentageChanges = getLatestPercentageChanges(
+    latestTraining,
+    trainings
+  );
+
   return (
     <main className="trainings">
       <section>
         {trainings
-          .map((training) => (
-            <TrainingTableWithButtons
-              key={training.id}
-              dispatch={dispatch}
-              training={training}
-              handleSetEditMode={() =>
-                handleSetEditMode({
-                  id: training.id,
-                  trainings,
-                  dispatch,
-                  textAreaRef,
-                })
-              }
-            />
-          ))
+          .map((training) => {
+            return (
+              <TrainingTableWithButtons
+                key={training.id}
+                dispatch={dispatch}
+                training={training}
+                percentageChanges={
+                  training.id === percentageChanges?.trainingId
+                    ? percentageChanges
+                    : null
+                }
+                handleSetEditMode={() =>
+                  handleSetEditMode({
+                    id: training.id,
+                    trainings,
+                    dispatch,
+                    textAreaRef,
+                  })
+                }
+              />
+            );
+          })
           .reverse()}
       </section>
     </main>
