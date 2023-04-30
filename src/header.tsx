7872a20e65354254340537bdccff66261ddbfbee
koeq <koe.be@web.de>
2023-04-30 19:14:25 +0200

    feat: add socials

--- src/header.tsx
+++ src/header.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { BiLogOut } from "react-icons/bi";
+import { IoLogoGithub } from "react-icons/io5";
 import { useAuth } from "./context";
 import { Hamburger } from "./hamburger";
 import "./styles/header.css";
@@ -40,14 +41,26 @@ interface Menu {
 export const Menu = ({ menuActive, logout }: Menu): JSX.Element => {
   return (
     <nav className={`menu ${menuActive ? "menu-active" : ""}`}>
-      <ul className="list">
+      <ul className="menu-list">
+        <hr />
         <li className="logout-container">
           <button onClick={logout}>
-            <span className="list-item-text"> Logout</span>
-            <BiLogOut size={"26"} className="logout-icon" />
+            <span className="menu-list-item-text">logout</span>
           </button>
         </li>
+        <hr />
+        <li>
+          <a href="https://github.com/koeq/gritlog">
+            <span className="menu-list-item-text">github</span>
+          </a>
+        </li>
+        <hr />
       </ul>
+      <div className="social">
+        <a href="https://github.com/koeq">
+          <IoLogoGithub fill="#9EA3A9" size={"26"} />
+        </a>
+      </div>
     </nav>
   );
 };
