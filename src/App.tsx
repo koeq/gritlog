32d8762ecabba715cb3993e2fb5ec64a84a78649
koeq <koe.be@web.de>
2023-05-01 18:24:37 +0200

    feat: load google script conditionally

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Suspense, lazy } from "react";
 import { useAuth } from "./context";
 import { Header } from "./header";
 import { LoadingSpinner } from "./loading-spinner";
+import { useGoogleScript } from "./use-google-script";
 
 const AuthedApp = lazy(() => import("./authed-app"));
 const UnauthedApp = lazy(() => import("./unauthed-app"));
 
 export const App = (): JSX.Element => {
   const { authed } = useAuth();
+  const googleScriptLoaded = useGoogleScript(authed === false);
 
   // We are still waiting for the status of authed.
   if (authed === undefined) {
@@ -17,7 +19,11 @@ export const App = (): JSX.Element => {
   return (
     <Suspense fallback={<LoadingSpinner />}>
       <Header authed={authed} />
-      {authed ? <AuthedApp /> : <UnauthedApp />}
+      {authed ? (
+        <AuthedApp />
+      ) : (
+        <UnauthedApp googleScriptLoaded={googleScriptLoaded} />
+      )}
     </Suspense>
   );
 };
