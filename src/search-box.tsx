5667027a71cd294c00da744a36af63f70302176f
koeq <koe.be@web.de>
2023-07-18 16:23:56 +0200

    feat: add search box disabled state

--- src/search-box.tsx
+++ src/search-box.tsx
@@ -1,14 +1,14 @@
 import { useEffect, useLayoutEffect, useRef, useState } from "react";
-import { IoCloseOutline, IoSearchOutline } from "react-icons/io5";
-import { useAuth, useTopLevelState } from "./context";
+import { IoCloseOutline, IoSearch } from "react-icons/io5";
+import { useTopLevelState } from "./context";
 import "./styles/search-box.css";
 import { debounce } from "./utils/debounce";
 
 export function SearchBox(): JSX.Element {
-  const { authed } = useAuth();
   const [active, setActive] = useState(false);
   const [{ trainings }, dispatch] = useTopLevelState();
   const inputRef = useRef<HTMLInputElement>(null);
+  const noTrainings = !trainings || trainings.length === 0;
 
   const handleClick = () => {
     if (!active && inputRef.current) {
@@ -47,15 +47,12 @@ export function SearchBox(): JSX.Element {
 
   return (
     <div className="search-box">
-      <button className="btn-search" onClick={handleClick}>
-        {authed &&
-          trainings &&
-          trainings.length > 0 &&
-          (active ? (
-            <IoCloseOutline size={22} />
-          ) : (
-            <IoSearchOutline size={19} />
-          ))}
+      <button
+        className={`btn-search${noTrainings ? " no-trainings" : ""}`}
+        onClick={handleClick}
+        disabled={noTrainings}
+      >
+        {active ? <IoCloseOutline size={22} /> : <IoSearch size={19} />}
       </button>
       <input
         ref={inputRef}
