ab381d224a97750926a1020e39a84f011e0f83ad
koeq <koe.be@web.de>
2023-09-08 14:48:40 +0200

    feat: push format info into input section

--- src/trainings.tsx
+++ src/trainings.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useRef, useState } from "react";
+import { useCallback, useRef } from "react";
 import "../src/styles/authed-app.css";
 import { AddEditCTAs } from "./add-edit-ctas";
 import { AddFirstTraining } from "./add-first-training";
@@ -7,7 +7,6 @@ import { BottomBarLayer } from "./bottom-bar-layer";
 import { useTopLevelState } from "./context";
 import { DeletionDialog } from "./deletion-dialog";
 import { filterTrainings } from "./filter-trainings";
-import { FormatInfo } from "./format-info";
 import { Layer } from "./layer";
 import { serializeExercises } from "./serialize-exercises";
 import { MemoizedTrainingList } from "./training-list";
@@ -20,7 +19,6 @@ interface TrainingsSectionProps {
 export const Trainings = ({
   trainings,
 }: TrainingsSectionProps): JSX.Element => {
-  const [showFormatInfo, setShowFormatInfo] = useState(false);
   const textAreaRef = useRef<HTMLTextAreaElement | null>(null);
   const [{ mode, showBottomBar, searchTerm }, dispatch] = useTopLevelState();
 
@@ -64,28 +62,14 @@ export const Trainings = ({
         handleSetEditMode={handleSetEditMode}
       />
 
-      {showFormatInfo && (
-        <Layer
-          clickHandler={() => {
-            setShowFormatInfo(false);
-            textAreaRef?.current?.focus();
-          }}
-        >
-          <FormatInfo />
-        </Layer>
-      )}
-
       {mode.type === "delete" && (
         <Layer>
           <DeletionDialog id={mode.id} />
         </Layer>
       )}
 
-      {showBottomBar && !showFormatInfo && <BottomBarLayer />}
-      <BottomBar
-        textAreaRef={textAreaRef}
-        setShowFormatInfo={setShowFormatInfo}
-      />
+      {showBottomBar && <BottomBarLayer />}
+      <BottomBar textAreaRef={textAreaRef} />
     </>
   );
 };
