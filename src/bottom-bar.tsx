17e3395bde58e732345d2a23935b7dd3e56bc398
koeq <koe.be@web.de>
2024-02-11 11:10:50 +0100

    update desktop styles

--- src/bottom-bar.tsx
+++ src/bottom-bar.tsx
@@ -5,10 +5,10 @@ import "./styles/bottom-bar.css";
 export const BottomBar = (): JSX.Element => {
   return (
     <nav id="bottom-bar">
-      <IoSettingsOutline size={19} />
-      <IoSearch size={19} />
-      <IoPencil size={19} />
-      <IoMdAdd color="#fff" size={24} strokeWidth={10} />
+      <IoSettingsOutline size={22} />
+      <IoSearch size={22} />
+      <IoPencil size={22} />
+      <IoMdAdd color="#fff" size={26} strokeWidth={10} />
     </nav>
   );
 };
