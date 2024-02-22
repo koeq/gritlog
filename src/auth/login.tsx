725dfa039f30e9cc97f550ecd145e10ec530d041
koeq <koe.be@web.de>
2024-02-22 23:07:08 +0100

    add new login view

--- src/auth/login.tsx
+++ src/auth/login.tsx
@@ -3,8 +3,16 @@ import "../styles/login.css";
 export const Login = (): JSX.Element => {
   return (
     <div className="login-container">
-      <h2>Log in to work out.</h2>
-      <div id="sign-in-with-google" />
+      <h1>gritlog</h1>
+      <p>
+        The text based <br /> stength training journal.
+      </p>
+      <hr />
+      {/* TODO: Trigger sign in on button click -> how? */}
+      <button>
+        <span id="login-text">Continue with Google</span>
+        <div id="sign-in-with-google" />
+      </button>
     </div>
   );
 };
