b6e6ce3b7c9571f1492d665041353289fd045454
koeq <koe.be@web.de>
2024-02-23 11:30:17 +0100

    use google sign in button

--- src/auth/login.tsx
+++ src/auth/login.tsx
@@ -8,11 +8,7 @@ export const Login = (): JSX.Element => {
         The text based <br /> stength training journal.
       </p>
       <hr />
-      {/* TODO: Trigger sign in on button click -> how? */}
-      <button>
-        <span id="login-text">Continue with Google</span>
-        <div id="sign-in-with-google" />
-      </button>
+      <div id="sign-in-with-google" />
     </div>
   );
 };
